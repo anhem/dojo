1488e9371d1552cef0cadf51e7cee881b74523e6
Andreas Hemmingsson <andhem@gmail.com>
2015-03-15 13:07:55 +0100

    MineField

---
+++ src/main/java/se/java/meetup/minefield/MineField.java
@@ -0,0 +1,13 @@
+package se.java.meetup.minefield;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MineField {
+
+    static final Logger LOG = LoggerFactory.getLogger(MineField.class);
+
+    public static void main(String[] args) {
+        LOG.info("Up and running!");
+    }
+}
