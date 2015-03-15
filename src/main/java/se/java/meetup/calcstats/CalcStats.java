84f19a8decd2763a4394594eb928bfea6c7b1e87
Andreas Hemmingsson <andhem@gmail.com>
2015-03-15 11:22:18 +0100

    calcstats

---
+++ src/main/java/se/java/meetup/calcstats/CalcStats.java
@@ -0,0 +1,13 @@
+package se.java.meetup.calcstats;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CalcStats {
+
+    static final Logger LOG = LoggerFactory.getLogger(CalcStats.class);
+
+    public static void main(String[] args) {
+        LOG.info("Up and running!");
+    }
+}
